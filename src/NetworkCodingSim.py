86047fb6b79f63d1c268170121aaa83e9d5a9fd1
Marco Gellecanao <marcogelle@berkeley.edu>
2022-03-27 17:49:09 -0700

    figure 5.15 left case code

--- src/NetworkCodingSim.py
+++ src/NetworkCodingSim.py
@@ -3,24 +3,25 @@ from graph import *
 # Constants
 R = 1
 
-# Initialize network topology
-node_s = Node("S")
-node_t = Node("T")
-node_u = Node("U")
-node_w = Node("W")
-node_x = Node("X")
-node_y = Node("Y")
-node_z = Node("Z")
+# Helper function to determine bits on links in the non-network coding case
+def populate_bits(src):
+    """
+    Fills in the bit values on each link in a multicasted, non-network coded network.
+    Inputs:
+        src (node): starting node of network
+    """
+    def fill_same_bits(node, bit):
+        for link in node.outgoing_links:
+            if link.bit == 0:
+                link.bit = bit
+                fill_same_bits(link.to_node, bit)
 
-create_link(node_s, node_t, R)
-create_link(node_s, node_u, R)
-create_link(node_t, node_w, R)
-create_link(node_t, node_y, R)
-create_link(node_u, node_w, R)
-create_link(node_u, node_z, R)
-create_link(node_x, node_y, R)
-create_link(node_x, node_z, R)
-create_link(node_w, node_x, R)
+    bit_num = 1
+    for link in src.outgoing_links:
+        if link.bit == 0:
+            link.bit = bit_num
+            fill_same_bits(link.to_node, bit_num)
+        bit_num += 1    
 
 # Simulute Multicast Throughput without Network Coding on above topology
 def simulate(src, dst_list, num_packets):
@@ -31,11 +32,25 @@ def simulate(src, dst_list, num_packets):
         src (node): source node
         dst_list (List[node]): list of destination nodes to be multicasted to
     Output:
-        (float) Maximum achievable throughput
+        (float) Maximum achievable network throughput
     """
-    bit = 1
-    for node in src.outgoing_links:
-        src.outgoing_links
+    if num_packets != len(src.outgoing_links):
+        print("Not supported currently :(")
+        return   
+
+    populate_bits(src)
+
+    # FIXME due to being sus: network throughput is average of dest node throughputs
+    network_throughput = 0
+    for dst_node in dst_list:
+        received_bits = set()
+        for in_link in dst_node.incoming_links:
+            received_bits.add(in_link.bit)
+        node_throughput = len(received_bits)
+        network_throughput += node_throughput
+    network_throughput /= len(dst_list)
+
+    return network_throughput
 
 # Simulute Multicast Throughput with Network Coding on above topology
 def simulate_NC(src, dst_list):
@@ -49,3 +64,47 @@ def simulate_NC(src, dst_list):
         (float) Maximum achievable throughput
     """
     pass
+
+def fig_5_15():
+    """
+    Creates network topology according to textbook figure 5.15. Initializes all link
+    bit values to 0.
+
+    Outputs: (src, dst_list, num_packets)
+        src (node): source node of network
+        dst_list (List[node]): list of destination nodes to be multicasted to
+        num_packets (int): number of bits/packets to be multicasted
+    """
+    node_s = Node("S")
+    node_t = Node("T")
+    node_u = Node("U")
+    node_w = Node("W")
+    node_x = Node("X")
+    node_y = Node("Y")
+    node_z = Node("Z")
+
+    create_link(node_s, node_t, R)
+    create_link(node_s, node_u, R)
+    create_link(node_t, node_w, R)
+    create_link(node_t, node_y, R)
+    create_link(node_u, node_w, R)
+    create_link(node_u, node_z, R)
+    create_link(node_x, node_y, R)
+    create_link(node_x, node_z, R)
+    create_link(node_w, node_x, R)
+
+    num_packets = 2
+    return node_s, [node_y, node_z], num_packets
+
+if __name__ == '__main__':
+    # Initialize network topology
+    source, dst_list, num_packets = fig_5_15()
+
+    # Simulate without network coding
+    net_throughput = simulate(source, dst_list, num_packets)
+    print("===== Without Network Coding =====")
+    print("Network throughput =", net_throughput)
+    source.print_network()
+    print()
+    print("===== With Network Coding =====")
+    print('not implemented')
--- src/graph.py
+++ src/graph.py
@@ -1,12 +1,45 @@
+from collections import deque
+
 class Node:
-
-    # incoming_links contains tuples; (destination, bit_amount (b1 or b2), link rate)
-    # outgoing_links contains tuples; (source, bit_amount (b1 or b2), link rate)
-
     def __init__(self, id):
         self.id = id
+
+        # both of the following are lists of Link objects
         self.incoming_links = []
         self.outgoing_links = []
+    
+    def __repr__(self):
+        return f"Node({self.id})"
+
+    def print_network(self):
+        """
+        Prints the network, assuming this is the source node.
+        Prints in order of breadth-first-search.
+        """
+        visited = set()
+        visited.add(self)
+        queue = deque([self])
+        while queue:
+            node = queue.popleft()
+            print(node)
+            for link in node.outgoing_links:
+                print("\t", link)
+                if link.to_node not in visited:
+                    queue.append(link.to_node)
+                    visited.add(link.to_node)
+
+class Link:
+    def __init__(self, from_node, to_node, rate):
+        self.from_node = from_node
+        self.to_node = to_node
+        self.rate = rate
+
+        # a bit value of 0 = unset value
+        # actual bit/packet ids will be integers >= 1
+        self.bit = 0
+    
+    def __repr__(self):
+        return f"Link(from={self.from_node.id}, to={self.to_node.id}, rate={self.rate}, bit={self.bit})"
 
 def create_link(src, dst, rate):
         """ Creates a link from src to dst, with a link rate.
@@ -15,15 +48,11 @@ def create_link(src, dst, rate):
             rate: link rate
         Outputs: None
         """
-        src.outgoing_links.append((dst, 0, rate))
-        dst.incoming_links.append((src, 0, rate))
+        link = Link(src, dst, rate)
+        src.outgoing_links.append(link)
+        dst.incoming_links.append(link)
 
 def update_incoming_link(src, dst):
    for link in dst.incoming_links:
     if src.id == link.id:
        link = (src, 1, )
-# class Network:
-#     def __init__(self, source_id):
-#         self.source = Node(source_id)
-
-    
